197a68e4f1b64ca14ebc59f36c599a186735e56c
Nadya <nadyatirta.page@gmail.com>
2025-06-13 20:34:10 +0700

    up sementara

--- src/connect.php
+++ src/connect.php
@@ -1,7 +1,23 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'projekpbw');
+$env = __DIR__ . '/../projekpbw.env';
 
-if ($conn->connect_error){
-    die("Connection failed: " . $con->connect_error);
+if (file_exists($env)) {
+    $env = parse_ini_file($env);
+    $host = $env['DB_HOST'];
+    $username = $env['DB_USER'];
+    $password = $env['DB_PASSWORD'];
+    $database = $env['DB_NAME'];
+} else {
+    $host = 'localhost';
+    $username = 'root';
+    $password = ''; 
+    $database = 'projekpbw';
+}
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
 ?>
+
---
+++ src/nav.php
@@ -0,0 +1,36 @@
+<!-- nav.php -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Sewa Motor</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <!-- Motor -->
+                <li class="nav-item">
+                    <a class="nav-link" href="daftar_motor.php">Daftar Motor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tambah_motor.php">Tambah Motor</a>
+                </li>
+
+                <!-- Pelanggan -->
+                <li class="nav-item">
+                    <a class="nav-link" href="daftar_pelanggan.php">Data Pelanggan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tambah_pelanggan.php">Tambah Pelanggan</a>
+                </li>
+
+                <!-- Transaksi -->
+                <li class="nav-item">
+                    <a class="nav-link" href="transaksi.php">Tambah Transaksi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="lihat_transaksi.php">Tampil Daftar Transaksi</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>

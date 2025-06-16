44c8d33bfb3e1819b774bcf4269914e325737aa2
unknown <rizkanadilaputri.com>
2025-06-16 15:49:41 +0700

    login

--- src/index.php
+++ src/index.php
@@ -1,11 +1,21 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Halaman Utama</title>
 </head>
 <body>
-    <h1></h1>Welcome to My PHP Application</h1>
+    <h1>Welcome, <?= $_SESSION['username']; ?>!</h1>
+    <p>Selamat datang di Aplikasi Sewa Motor.</p>
+    <a href="logout.php">Logout</a>
 </body>
 </html>
